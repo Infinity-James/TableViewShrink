3ac0da78610c0a426392280c7820aa2188dc85b2
James Valaitis <james@infinityjames.com>
2017-06-24 12:08:19 +0100

    Initial commit.

--- TableViewShrink/ViewController.swift
+++ TableViewShrink/ViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+//  MARK: View Controller
+final class ViewController: UIViewController {
+	//  MARK: Constant
+	fileprivate let maximumOffset: CGFloat = 120.0
+	fileprivate let cellReuseIdentifier = "Cell"
+	fileprivate let data = [
+		"Oh dear.",
+		"What the fuck",
+		"have we here?",
+		"These motherfuckers,",
+		"all thorn no rose.",
+		"Egads, what a bad dream;",
+		"better not sleep.",
+		"Take a No-Doz,",
+		"do some blow.",
+		"Oh shit.",
+		"What the hell",
+		"have we done?",
+		"It's alive,",
+		"and it's hungry as fuck."
+	]
+	//  MARK: Properties
+	@IBOutlet fileprivate weak var topTableViewConstraint: NSLayoutConstraint!
+	@IBOutlet fileprivate weak var leadingTableViewConstraint: NSLayoutConstraint!
+	@IBOutlet fileprivate weak var trailingTableViewConstraint: NSLayoutConstraint!
+	@IBOutlet fileprivate weak var tableView: UITableView!
+}
+//  MARK: View Lifecycle
+extension ViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		var inset = tableView.contentInset
+		inset.top = maximumOffset
+		tableView.contentInset = inset
+		
+		var offset = tableView.contentOffset
+		offset.y = 0.0
+		tableView.contentOffset = offset
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
 }
-
+//  MARK: UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+		let title = data[indexPath.row]
+		cell.textLabel?.text = title
+		return cell
+	}
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return data.count
+	}
+}
+//  MARK: UIScrollViewDelegate
+extension ViewController: UIScrollViewDelegate {
+	func scrollViewDidScroll(_ scrollView: UIScrollView) {
+		let yOffset = min(0.0, max(-maximumOffset, scrollView.contentOffset.y))
+		let constant = -yOffset
+		topTableViewConstraint.constant = constant
+		leadingTableViewConstraint.constant = constant / 5.0
+		trailingTableViewConstraint.constant = constant / 5.0
+		view.layoutIfNeeded()
+	}
+}
